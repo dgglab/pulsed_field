067920a8078cc50ab861b9eb69a2a07a9cc36b8f
aaronsharpe <aaronlsharpe@gmail.com>
2018-12-18 16:45:14 -0800

    further formating

--- modules/pulse_processing.py
+++ modules/pulse_processing.py
@@ -111,11 +111,11 @@ def rise_fall(shot, params, thresh=0.05):
         shot[p+'_rising'] = shot[p][threshind_rising:index_max]
         shot[p+'_falling'] = shot[p][index_max:index_max + threshind_falling]
     
-    shot['Rxx_rising'] = shot['Vxx_rising']/shot['I_rising']
-    shot['Rxx_falling'] = shot['Vxx_falling']/shot['I_falling']
+    shot['Rxx_rising'] = shot['Vxx_rising'] / shot['I_rising']
+    shot['Rxx_falling'] = shot['Vxx_falling'] / shot['I_falling']
     
-    shot['Rxy_rising'] = shot['Vxy_rising']/shot['I_rising']
-    shot['Rxy_falling'] = shot['Vxy_falling']/shot['I_falling']
+    shot['Rxy_rising'] = shot['Vxy_rising'] / shot['I_rising']
+    shot['Rxy_falling'] = shot['Vxy_falling'] / shot['I_falling']
     
 
 def interp_shots(shots, params):
@@ -138,37 +138,35 @@ def interp_shots(shots, params):
     minB_shot = np.argmin(B_max_store)
     
     for (i,shot),(p,_) in product(enumerate(shots), params):
-        if i == minB_shot:
-            pass 
-        else:
-            if p == 'B':
-                pass
-            else:
+        if i != minB_shot:
+            if p != 'B':
                 shot[p+'_rising'] = np.interp(
-                    np.abs(shots[minB_shot]['B_rising']), np.abs(shot['B_rising']),
+                    np.abs(shots[minB_shot]['B_rising']), 
+                    np.abs(shot['B_rising']),
                     shot[p + '_rising'])
                     
                 shot[p+'_falling'] = np.interp(
-                    np.abs(shots[minB_shot]['B_falling']), np.abs(np.flip(shot['B_falling'])),
+                    np.abs(shots[minB_shot]['B_falling']), 
+                    np.abs(np.flip(shot['B_falling'])),
                     np.flip(shot[p + '_falling']))
-     
-    
+                    
     for i,shot in enumerate(shots):
         if i == minB_shot:
             shot['B_rising'] = np.abs(shot['B_rising'])
             shot['B_falling'] = np.abs(shot['B_falling'])
-            pass
         else:
             shot['B_rising'] = np.interp(
-                np.abs(shots[minB_shot]['B_rising']), np.abs(shot['B_rising']),
+                np.abs(shots[minB_shot]['B_rising']),
+                np.abs(shot['B_rising']),
                 np.abs(shot['B_rising']))
                 
             shots[i]['B_falling'] = np.interp(
-                np.abs(shots[minB_shot]['B_falling']), np.abs(np.flip(shot['B_falling'])),
+                np.abs(shots[minB_shot]['B_falling']),
+                np.abs(np.flip(shot['B_falling'])),
                 np.abs(np.flip(shot['B_falling'])))
                 
-            shot['Rxx_rising'] = shot['Vxx_rising']/shot['I_rising']
-            shot['Rxx_falling'] = shot['Vxx_falling']/shot['I_falling']
+            shot['Rxx_rising'] = shot['Vxx_rising'] / shot['I_rising']
+            shot['Rxx_falling'] = shot['Vxx_falling'] / shot['I_falling']
             
-            shot['Rxy_rising'] = shot['Vxy_rising']/shot['I_rising']
-            shot['Rxy_falling'] = shot['Vxy_falling']/shot['I_falling']
+            shot['Rxy_rising'] = shot['Vxy_rising'] / shot['I_rising']
+            shot['Rxy_falling'] = shot['Vxy_falling'] / shot['I_falling']
